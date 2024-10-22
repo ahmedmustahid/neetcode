8bc1d2fd90bd84ef22160843b77ede40c1f3fd79
ahmedmustahid <einsteinium25@gmail.com>
2024-10-23 06:07:55 +0900

    minor changes

--- 2sum2.cpp
+++ 2sum2.cpp
@@ -15,51 +15,20 @@ void printVec(const vector<T> &vec)
 
 vector<int> ts2(vector<int> &numbers, int target)
 {
-
-    auto it = std::upper_bound(numbers.begin(), numbers.end(), target);
-    numbers.assign(numbers.begin(), it);
-
-    printVec(numbers);
-
     int maxId = numbers.size() - 1;
     int minId = 0;
 
-    int targetSubMin = target - numbers[minId];
-    int targetSubMax = target - numbers[maxId];
-
     while (maxId != minId)
     {
-        numbers.assign(numbers.begin() + minId, numbers.begin() + maxId + 1);
-
-        bool isTargetSubMinPresent = std::binary_search(numbers.begin(), numbers.end(), targetSubMin);
-        if (targetSubMin > target)
-            minId++;
-        else if (!isTargetSubMinPresent)
-            minId++;
-        else if (isTargetSubMinPresent)
+        int sum = numbers[maxId] + numbers[minId];
+        if (sum > target)
+            --maxId;
+        else if (sum < target)
+            ++minId;
+        else
         {
-            auto it2 = std::lower_bound(numbers.begin(), numbers.end(), targetSubMin);
-            int targetSubMinPos = static_cast<int>(it2 - numbers.begin());
-
-            return vector<int>({minId, targetSubMinPos});
+            return vector<int>({minId + 1, maxId + 1});
         }
-
-        targetSubMin = target - numbers[minId];
-
-        bool isTargetSubMaxPresent = std::binary_search(numbers.begin(), numbers.end(), targetSubMax);
-        if (targetSubMax > target)
-            maxId--;
-        else if (!isTargetSubMaxPresent)
-            maxId--;
-        else if (isTargetSubMaxPresent)
-        {
-            auto it2 = std::lower_bound(numbers.begin(), numbers.end(), targetSubMax);
-            int targetSubMaxPos = static_cast<int>(it2 - numbers.begin());
-
-            return vector<int>({maxId, targetSubMaxPos});
-        }
-
-        targetSubMax = target - numbers[maxId];
     }
 
     return {};
@@ -68,8 +37,10 @@ vector<int> ts2(vector<int> &numbers, int target)
 int main()
 {
 
-    vector<int> v{1, 3, 4, 5, 7, 10, 11};
-    int target = 9;
+    // vector<int> v{1, 3, 4, 5, 7, 10, 11};
+    // int target = 9;
+    vector<int> v{-1, 0, 0, 2, 3, 4};
+    int target = 0;
     vector<int> v2 = ts2(v, target);
     printVec(v2);
 }
